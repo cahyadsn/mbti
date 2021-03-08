4b751b9ecd836d725955b63e86b9c86f90443a7b
Cahya DSN <cahyadsn@gmail.com>
2021-03-08 15:48:05 +0700

    Update result.php
    mengubah statement license ke MIT license

--- result.php
+++ result.php
@@ -1,11 +1,30 @@
 <?php
-/************************************
-FILENAME     : result.php
-AUTHOR       : CAHYA DSN
-CREATED DATE : 2015-01-14
-DEMO SITE    : http://phpindonesia.id1945.com/mbti
-SOURCE CODE  : https://github.com/cahyadsn/mbti
-*************************************/
+/*
+BISMILLAAHIRRAHMAANIRRAHIIM - In the Name of Allah, Most Gracious, Most Merciful
+================================================================================
+filename 	  : result.php
+purpose  	  : result application page
+create   	  : 2015-01-14
+last edit	  : 2021-03-08
+author   	  : cahya dsn
+demo site 	: https://psycho.cahyadsn.com/mbti
+soure code 	: https://github.com/cahyadsn/mbti
+================================================================================
+This program is free software; you can redistribute it and/or modify it under the
+terms of the MIT License.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+See the MIT License for more details
+
+copyright (c) 2021 by cahya dsn; cahyadsn@gmail.com
+================================================================================*/
 header('Expires: '.date('r'));
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', FALSE);
